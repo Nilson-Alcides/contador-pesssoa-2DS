3ce3b899ab108764104520a8ca589cf756d5e0fe
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-09 19:49:06 -0300

    Update Projeto

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(
+  MaterialApp(
+    home: Container(
+      color: Colors.blue,
+      alignment: Alignment.center,    
+      child: Text("Hello World"),
+    ),
+  ),
+);
+}
