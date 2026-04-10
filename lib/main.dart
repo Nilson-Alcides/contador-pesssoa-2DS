1d32707c63fc13ad4cd6a0b4d11e2a4246cea234
Nilson-Alcides <nilson_alcides@hotmail.com>
2026-04-09 21:03:37 -0300

    update Hello World

--- lib/main.dart
+++ lib/main.dart
@@ -11,3 +11,25 @@ runApp(
   ),
 );
 }
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext content){
+    return MaterialApp(     
+      home: HomePage(), 
+  );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      alignment: Alignment.center,
+      child: Text("Hello World"),
+    );
+  }
+}
